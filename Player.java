e12867bf39f9647d422ad16586218cd9b0873c3e
Vu Huy Nguyen <vuhuynguyen4@gmail.com>
2022-01-11 18:57:10 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,21 +4,33 @@ package main;
 */
 public class Player {
 
-	static int reputation;
+	static int reputation; // Das ist ein Ruf variabel. Es berechnet, wie berühmt unser Figur ist.
+
 	public Player() {
-		reputation = 50;
+		reputation = 50; // Am Anfang beträgt der Ruf 50.
 	}
 	
-	public static int getReputation() {return reputation;}
-	public static void setReputation(int a) {reputation = a;}
+	public static int getReputation() {return reputation;} // Getter Methode für den Ruf
+	public static void setReputation(int a) {reputation = a;} // Setter Methode für den Ruf
 	
 	public static void increaseReputation(int amount) {
+		// Diese Methode hat ein Amount Argument.
+		// Der Ruf nimmt um den Betrag des Amounts zu.
+		System.out.println("Dein Ruf steigt um " + amount);
 		setReputation(reputation + amount);
 		System.out.println("Deine neue Ruf ist " + reputation);
 	}
-	//TODO: write check and endgame for reputation when it drops below 0
+
+	
 	public static void decreaseReputation(int amount) {
+		// Gleiche Methode wie vorher, aber hier nimmt der Ruf ab.
+		System.out.println("Dein Ruf sinkt um " + amount);
 		setReputation(reputation - amount);
 		System.out.println("Deine neue Ruf ist " + reputation);
+		if (reputation <= 0) {
+			System.out.println("Ihr Ruf hat Null erreicht");
+			System.out.println("Die Stadtbewohner haben keinen Respekt vor dir und wünschen dir den Tod. Du wirst vom Richter zum Tode verurteilt. Game over.");
+			System.exit(0); // Endet das Programm, Game Over
+		}
 	}
 }
