e4152f424d3b035df44b56ad763866841a4aa4e5
Vu Huy Nguyen <vuhuynguyen4@gmail.com>
2022-01-11 17:28:19 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,24 @@
+package main;
+/*
+	Diese Klasse kontrolliert die Informationen uber den Hauptfigur
+*/
 public class Player {
-   public Player() {
-       
-   } 
+
+	static int reputation;
+	public Player() {
+		reputation = 50;
+	}
+	
+	public static int getReputation() {return reputation;}
+	public static void setReputation(int a) {reputation = a;}
+	
+	public static void increaseReputation(int amount) {
+		setReputation(reputation + amount);
+		System.out.println("Deine neue Ruf ist " + reputation);
+	}
+	//TODO: write check and endgame for reputation when it drops below 0
+	public static void decreaseReputation(int amount) {
+		setReputation(reputation - amount);
+		System.out.println("Deine neue Ruf ist " + reputation);
+	}
 }
