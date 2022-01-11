1e640903c29101384cca83653a3f9cc9a6c029a3
Vu Huy Nguyen <vuhuynguyen4@gmail.com>
2022-01-11 17:27:57 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+package main;
+
+/*
+ * Diese Klasse ist fur technische Anweisungen, z.B. wenn wir ein input vom Benutzer lesen müssen.
+ */
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Something");
-    }
+
+	public static Scanner scanner;
+	
+	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+		Text.anfangstext();
+		Text.kapitel1();
+		Text.kapitel2();
+		Text.kapitel3();
+		
+
+	}
 }
