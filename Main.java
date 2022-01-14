58efd2744ce6ffcb515668e3c8f32969075d574f
Vu Huy Nguyen <vuhuynguyen4@gmail.com>
2022-01-14 12:02:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 package main;
 
-/* @author: Huy Vu, Pedro, Gustavo 
+/** Basisklasse: Diese Klasse ausführen, um den Spiel zu spielen
+ * Diese Klasse ist für technische Anweisungen: die Methoden für den ganzen Programm (die Geschichte) aufzurufen 
+ * und auch die wichtige Einstellungen machen
+ * @author: Huy Vu, Pedro, Gustavo 
  * @date: 11.01.2022
  * Informatik Projekt T442 STK
  */
 import java.util.Scanner;
 
-// Diese Klasse ist fur technische Anweisungen, z.B. wenn wir ein input vom Benutzer lesen mï¿½ssen.
 public class Main {
 
-	public static Scanner scanner;
+	public static Scanner scanner; //Scanner für andere Klasse einstellen, andere Klasse benutzen das mit Main.scanner
 	
 	public static void main(String[] args) {
 		scanner = new Scanner(System.in);
@@ -23,4 +25,3 @@ public class Main {
 		scanner.close();	
 	}
 }
-
